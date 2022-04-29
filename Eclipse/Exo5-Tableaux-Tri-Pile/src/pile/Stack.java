b88eda5b09ca41fa0320470504a8f211e2756f13
isi <isi@751FJW2>
2022-04-29 12:05:09 -0400

    Exo Base renommage

--- Eclipse/Exo5-Tableaux-Tri-Pile/src/pile/Stack.java
+++ Eclipse/Exo5-Tableaux-Tri-Pile/src/pile/Stack.java
@@ -4,62 +4,65 @@ public class Stack {
 
 	private int[] data;
 	private int count;
+
 	public Stack() {
 		// TODO Auto-generated constructor stub
 		this.data = new int[1];
 	}
-	
+
 	public int getCount() {
 		return this.count;
 	}
-	
+
 	public int getCapacity() {
 		return this.data.length;
 	}
-	
+
 	public boolean isEmpty() {
-		return this.count==0;
+		return this.count == 0;
 	}
+
 	public boolean isFull() {
-		return this.count==this.data.length;
+		return this.count == this.data.length;
 	}
-	
+
 	public void resize(int taille) {
-		int[] newData = new int[taille];
-		int i=0;
-		for( i=0; i<this.count; i++) {
-			newData[i]=this.data[i];
+		if (taille >= this.count) {
+			int[] newData = new int[taille];
+			int i = 0;
+			for (i = 0; i < this.count; i++) {
+				newData[i] = this.data[i];
+			}
+
+			this.data = newData;
 		}
-		
-		this.data = newData;
-	
 	}
-	
+
 	public void push(int val) {
-		
-		if(this.isFull()) {
-			 this.resize(this.count*2);
+
+		if (this.isFull()) {
+			this.resize(this.count * 2);
 		}
-		
-		this.data[this.count]=val;
-		this.count+=1;
+
+		this.data[this.count] = val;
+		this.count += 1;
 	}
-	
+
 	public void printData() {
-		for(int v:this.data) {
-			System.out.print(v+ " ");
+		for (int v : this.data) {
+			System.out.print(v + " ");
 		}
 		System.out.println();
 	}
-	
+
 	public int pop() {
-		int res = this.data[this.count-1];
-		this.data[this.count-1]=0;
-		this.count-=1;
-		if((this.count <= this.data.length/2)&&(this.count>=1)) {
-			 this.resize(this.data.length/2);
+		int res = this.data[this.count - 1];
+		this.data[this.count - 1] = 0;
+		this.count -= 1;
+		if ((this.count <= this.data.length / 2) && (this.count >= 1)) {
+			this.resize(this.data.length / 2);
 		}
-		return res ;
+		return res;
 	}
 
 }
