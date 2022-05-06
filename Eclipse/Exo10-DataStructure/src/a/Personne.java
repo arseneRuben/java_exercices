9ec207c405a4577ffd5975afcafe2f5335cd7ead
isi <isi@751FJW2>
2022-05-06 11:30:08 -0400

    Classe Personne, HashTable

--- Eclipse/Exo10-DataStructure/src/a/Personne.java
+++ Eclipse/Exo10-DataStructure/src/a/Personne.java
@@ -1,6 +1,7 @@
 package a;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 
 
@@ -28,19 +29,36 @@ public class Personne {
 	}
 	
 	public String toString() {
-		return " ("+ this.id+ ")"+this.fullname + "\n";
+		return "\n ("+ this.id+ ")"+this.fullname ;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Question 2
+		ArrayList<Personne> arrayList = new ArrayList<Personne>();
+		Personne p1=new Personne("Alain Herzt"), p2=new Personne("Francois Capone"), 
+				p3=new Personne("Kamto Maurice"), p4 = new Personne("Gabriella Nicolescu"),
+				p5 = new Personne("Bonde Lossan"), p6 = new Personne("Giles Pesant");
 		
-		ArrayList<Personne> list = new ArrayList<Personne>();
-		list.add(new Personne("Alain Herzt"));
-		list.add(new Personne("Francois Capone"));
-		list.add(new Personne("Kamto Maurice"));
-		list.add(new Personne("Gabriella Nicolescu"));
-		list.add(new Personne("Giles Pesant"));
-		System.out.println(list);
+		arrayList.add(p1);
+		arrayList.add(p2);
+		arrayList.add(p3);
+		arrayList.add(p4);
+		arrayList.add(p5);
+		arrayList.add(p6);
+		System.out.println(arrayList);
+		
+	    // Question 3
+		 Hashtable<Personne, Integer> hashTable
+	     = new Hashtable<Personne, Integer>();
+		 hashTable.put(p1, p1.getId());
+		 hashTable.put(p2, p2.getId());
+		 hashTable.put(p3, p3.getId());
+		 hashTable.put(p4, p4.getId());
+		 hashTable.put(p5, p5.getId());
+		 hashTable.put(p6, p6.getId());
+		 System.out.println(hashTable);
+		 
 			
 			
 
